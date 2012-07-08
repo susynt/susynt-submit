a061cf497687f313891a38f315967dce48c625a8
Steven Farrel <sfarrell@cern.ch>
2012-07-08 22:08:03 +0000

    update counter submission script

--- submitCounter.sh
+++ submitCounter.sh
@@ -2,25 +2,21 @@
 
 iteration=i2
 
-# get the samples of interest
-if [[ $# = 0 ]]; then
-        echo "submit all samples"
-        pattern="mc12"
-else
-        pattern=$1
+#
+# Submit counter job for all mc datasets in input file passed as argument
+#
+
+if [[ $# < 1 ]]; then
+        echo "Pass DS file as input argument"
+        exit 1
 fi
-matches=(`cat mcSamples.txt | grep $pattern | tr '\t' ','`)
-echo "${#matches[@]} matches"
 
 # set it up manually I guess
 #source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalPandaClientSetup.sh
 
 # Loop over samples
-for line in ${matches[@]}; do
+for inDS in `cat $1`; do
 
-        info=(`echo $line | tr ',' ' '`)
-        #sample=${info[0]}
-        inDS=${info[0]}
         sample=${inDS#mc12_8TeV.*.}
         sample=${sample%.merge.*/}
 
