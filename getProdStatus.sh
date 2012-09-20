a4affc00373e98b6e8794f62b865fcdf42c89947
Steven Farrel <sfarrell@cern.ch>
2012-09-20 19:51:54 +0000

    add script to get ami prod status

---
+++ getProdStatus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script uses the Tier3 AMI software interface to print the prodSysStatus of a list of datasets in an input file
+# Setup AMI and voms proxy prior to running this script
+
+# Pass input file with list of datasets
+if [[ $# < 1 ]]; then
+        echo "Usage:"
+        echo " > getProdStatus.sh <DATASET-FILE>"
+        exit 1
+fi
+
+for ds in `cat $1`; do
+
+        prodStatus=`ami dataset info $ds | grep prodsysStatus | awk '{print $2,$3,$4}'`
+
+        echo $ds
+        echo -e "        $prodStatus"
+
+done
