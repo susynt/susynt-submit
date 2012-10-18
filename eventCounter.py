467178a85e2751c9a32becd8ffee435b5bc2b7e6
Steven Farrel <sfarrell@cern.ch>
2012-10-18 15:25:36 +0000

    update event counter

--- eventCounter.py
+++ eventCounter.py
@@ -10,29 +10,39 @@ from ROOT import *
 def main():
 
     if len(sys.argv) < 2:
-        print "usage:  eventCounter file1,file2,..."
+        print 'usage:  eventCounter file1,file2,...'
         return
 
-    print "eventCounter begin"
+    print 'eventCounter begin'
 
     # parse files
     files = sys.argv[1].split(',')
-    print "files:", files
+    print 'files:', files
 
-    # build chain
-    chain = TChain("susyMeta/CutFlowTree")
+    # d3pd chain
+    d3pdChain = TChain('susy')
+    # meta data chain
+    metaChain = TChain('susyMeta/CutFlowTree')
+
     for file in files:
-        chain.Add(file)
-    nEntries = chain.GetEntries()
-    chain.ls()
-    print "total entries:", nEntries
+        d3pdChain.Add(file)
+        metaChain.Add(file)
+    d3pdEntries = d3pdChain.GetEntries()
+    metaEntries = metaChain.GetEntries()
+    metaChain.ls()
+    print 'total d3pd entries:', d3pdEntries
+    print 'total meta entries:', metaEntries
 
-    # output root file and histo
-    f = TFile("sumWeights.root", "recreate")
-    h = TH1F("sumw", "sumw", 1, 0, 2)
+    # output root file and histos
+    f = TFile('sumWeights.root', 'recreate')
+    hRaw = TH1F('rawEntries', 'rawEntries', 1, 0, 2)
+    h = TH1F('sumw', 'sumw', 1, 0, 2)
+
+    # Take raw entries directly from chain
+    hRaw.Fill(1, d3pdEntries)
 
     # loop over susy metadata tree
-    for entry in chain:
+    for entry in metaChain:
 
         names        = [name for name in entry.name]
         descriptions = [desc for desc in entry.description]
@@ -41,18 +51,26 @@ def main():
         # loop over filters
         for name, desc, nEvent in zip(names, descriptions, nEvents):
             
-            if name == "AllExecutedEvents" and desc == "Nb of executed events before any cut":
+            print name, nEvent
+            if name == 'AllExecutedEvents' and desc == 'Nb of executed events before any cut':
             
-                print name, nEvent
                 h.Fill(1, nEvent)
 
+                # At least in data, there can be more than one 'AllExecutedEvents', so only save one
+                break
+
+
+    # Print summary
+    print 'count results:'
+    print '  d3pd', hRaw.GetBinContent(1)
+    print '  sumw', h.GetBinContent(1)
 
     # Write out the histo
     h.Write()
     f.Close()
 
-    print "eventCounter end"
+    print 'eventCounter end'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
