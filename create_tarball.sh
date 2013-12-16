9d6fe3307bd04baaba77304aa86e32d331a8f3ef
Steven Farrel <sfarrell@cern.ch>
2013-12-16 16:47:22 +0000

    make svn info check before building tarball

--- create_tarball.sh
+++ create_tarball.sh
@@ -11,6 +11,9 @@ if [ -e $tarFile ]; then
     rm $tarFile
 fi
 
+# Create a snapshot list of the SVN tags used to make this tarball, useful for when you forget
+svn info $ROOTCOREDIR/../SusyNtuple $ROOTCOREDIR/../SusyCommon $ROOTCOREDIR/../MultiLep $ROOTCOREDIR/../SUSYTools > tarBallTags 
+
 # We don't need to actually submit a job, but still need to provide some dummy arguments to prun
 prun --exec "echo" --tmpDir=$tmpDir --outTarBall=$tarFile --useRootCore --noSubmit --athenaTag=17.0.5.5 --outDS=user.$nickname.DummyDoesNothing
 
