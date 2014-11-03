754215ee4da8418c10af2740a1d4423524bda06e
gerbaudo <davide.gerbaudo@gmail.com>
2014-11-04 00:23:12 +0100

    analysis release (seems to mostly work)

--- bash/create_tarball.sh
+++ bash/create_tarball.sh
@@ -4,7 +4,7 @@ nickname=$USER
 tmpDir=/tmp/$USER
 mkdir -p $tmpDir
 
-tarFile=area.tar
+tarFile=area.tgz
 
 if [ -e $tarFile ]; then
     echo "Removing old tar file"
@@ -12,8 +12,8 @@ if [ -e $tarFile ]; then
 fi
 
 # Create a snapshot list of the SVN tags used to make this tarball, useful for when you forget
-svn info $ROOTCOREDIR/../SusyNtuple $ROOTCOREDIR/../SusyCommon $ROOTCOREDIR/../MultiLep $ROOTCOREDIR/../SUSYTools > tarBallTags 
+svn info $ROOTCOREBIN/../SusyNtuple $ROOTCOREBIN/../SusyCommon $ROOTCOREBIN/../MultiLep $ROOTCOREBIN/../SUSYTools > tarBallTags
 
 # We don't need to actually submit a job, but still need to provide some dummy arguments to prun
-prun --exec "echo" --tmpDir=$tmpDir --outTarBall=$tarFile --useRootCore --noSubmit --athenaTag=17.0.5.5 --outDS=user.$nickname.DummyDoesNothing
+prun --exec "echo" --tmpDir=$tmpDir --outTarBall=$tarFile --useRootCore --noSubmit --outDS=user.$nickname.DummyDoesNothing
 
